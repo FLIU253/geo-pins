910ae8236c524ded94e8a12a86ac9c4ad5d9fc31
Franklin Liu <franklin.liu@autodata.net>
2019-10-25 11:15:30 -0400

    street map implemented

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGl, { NavigationControl } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGl
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiZmxpdTI1MyIsImEiOiJjanl1NGFuNHIwNGRwM25sMmh0M2N4ajJiIn0.ryfp0I-Dt9Ky8HkrqJ5rmQ"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        {...viewport}
+      >
+        {/* Navigation Control  */}
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+          />
+        </div>
+      </ReactMapGl>
+    </div>
+  );
 };
 
 const styles = {
