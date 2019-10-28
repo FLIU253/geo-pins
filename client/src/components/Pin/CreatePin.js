321565fef0c8af0452ed35295d670c504b7aaa0e
Franklin Liu <franklin.liu@autodata.net>
2019-10-28 12:56:59 -0400

    subscribing to live data added using apollo client and subscription (pubsub) of graphql

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -51,13 +51,8 @@ const CreatePin = ({ classes }) => {
       const { latitude, longitude } = state.draft;
       const variables = { title, image: url, content, latitude, longitude };
 
-      const { createPin } = await client.request(
-        CREATE_PIN_MUTATION,
-        variables
-      );
+      await client.request(CREATE_PIN_MUTATION, variables);
 
-      console.log("Pin created", { createPin });
-      dispatch({ type: "CREATE_PIN", payload: createPin });
       handleDeleteDraft();
     } catch (err) {
       setSubmitting(false);
