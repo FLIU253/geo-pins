00691a346b270390d999b5e00ed1375b3f4e18c4
Franklin Liu <franklin.liu@autodata.net>
2019-10-28 11:23:25 -0400

    Adding comments and displaying comments functionality fully implemented

--- client/src/components/Comment/Comments.js
+++ client/src/components/Comment/Comments.js
@@ -6,30 +6,36 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
 
-const Comments = ({ classes, comments }) => (
-  <List className={classes.root}>
-    {comments.map((comment, i) => (
-      <ListItem key={i} alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar src={comment.author.picture} alt={comment.author.name} />
-        </ListItemAvatar>
-        <ListItemText
-          primary={comments.text}
-          secondary={
-            <Typography
-              className={classes.inline}
-              component="span"
-              color="textPrimary"
-            >
-              {comment.author.name}
-            </Typography>
-          }
-        />
-      </ListItem>
-    ))}
-  </List>
-);
+const Comments = ({ classes, comments }) => {
+  return (
+    <List className={classes.root}>
+      {comments.map((comment, i) => (
+        <ListItem key={i} alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar src={comment.author.picture} alt={comment.author.name} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={comment.text}
+            secondary={
+              <>
+                <Typography
+                  className={classes.inline}
+                  component="span"
+                  color="textPrimary"
+                >
+                  {comment.author.name}
+                </Typography>
+                . {distanceInWordsToNow(Number(comment.createdAt))} ago
+              </>
+            }
+          />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
 const styles = theme => ({
   root: {
